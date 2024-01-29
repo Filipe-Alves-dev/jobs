f9b19a12eb7dbe2165f0f3467d79ff929300d208
filipe.cardoso <alvesfilipe87@gmail.oom>
2024-01-29 10:48:09 -0300

    att curso condições aninhadas

--- curso_python/desafio 45.py
+++ curso_python/desafio 45.py
@@ -4,10 +4,19 @@ print('\033[1;33m-=-\033[m' * 20)
 print('\033[1;36m                         JOKENPÔ\033[m')
 print('\033[1;33m-=-\033[m' * 20)
 print(emojize('Escolha entre PEDRA, PAPEL e TESOURA e tente me vencer... :pedra:  :rolo_de_papel: :tesoura:', language='pt'))
-escolha = int(input('Digite :\n1 - Para PEDRA\n2 - Para PAPEL\n3 - Para PEDRA\n: '))
+escolha = int(input(emojize('Digite :\n1 - Para :pedra:\n2 - Para :rolo_de_papel:\n3 - Para :tesoura:\n: ', language='pt')))
 npc = randint(1,3)
 if npc == escolha:
     print('Nós escolhemos o mesmo e acabamos empatando!')
 elif escolha == 1 and npc == 2:
-    print(emojize('HAHAHA Ganhei de você escolhi papel! :rolo_de_papel:', language='pt'))
-    print(npc)
+    print(emojize('HAHAHA Ganhei de você escolhi :rolo_de_papel:!"', language='pt'))
+elif escolha == 2 and npc == 3:
+    print(emojize('HAHAHA Ganhei de você escolhi :tesoura:!', language='pt'))
+elif escolha == 3 and npc == 1:
+    print(emojize('HAHAH Ganhei de você escolhi :pedra:!', language='pt'))
+elif escolha == 1 and npc == 3:
+    print(emojize('Meus parabéns você me venceu! Eu escolhi :tesoura:!', language='pt'))
+elif escolha == 2 and npc == 1:
+    print(emojize('Meus parabéns você me venceu! Eu escolhi :pedra: ', language='pt'))
+elif escolha == 3 and npc == 2:
+    print(emojize('Meus parabéns você me venceu! Eu escolhi :rolo_de_papel:', language='pt'))
