c2bc35f2c9ab5ae3d71b976d333ff58676a7df2b
Filipe Alves <117540806+Filipe-Alves-dev@users.noreply.github.com>
2024-10-12 18:54:06 -0300

    att 12-10

--- curso_python/desafio 65.py
+++ curso_python/desafio 65.py
@@ -1,10 +1,14 @@
-n = 0
-cont = 1
+lista = []
+cont = 0
 total = 0
 escolha = 'S'
 while escolha == 'S':
-    n = int(input('Digite um número :')) 
+    n = int(input('\nDigite um número : ')) 
     cont+=1
     total+=n
-    escolha = str(input('Quer continuar S/N?')).upper()
-print(f'Você digitou {cont} números e a média foi de {total/cont} o maior valor foi  e o menor valor foi .')
+    escolha = str(input('\nQuer continuar S/N? : ')).upper()
+    if escolha != 'N' and escolha != 'S':
+        escolha = str(input('\nDigitou um caractere invalido. Quer continuar S/N? ')).upper()
+    lista += [n]
+print(f'\nVocê digitou {cont} números e a média foi de {total/cont} o maior valor foi {max(lista)} e o menor valor foi {min(lista)}. ')
+print(n)
