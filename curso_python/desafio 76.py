515f75fea2cff067d33d4b73e223b0b8131c7765
Filipe-Alves-dev <filipecardosoass@gmail.com>
2024-10-18 10:49:41 -0300

    att 18-10-24

--- curso_python/desafio 76.py
+++ curso_python/desafio 76.py
@@ -1,2 +1,11 @@
-list_tupla = ('Refrigerante 2l', 10, 'Suco', 5, 'Café com leite', 3, 'Café', 2.5, 'Pastel', 3.5, 'Coxinha', 3, 'Enrolado de salsicha', 3, 'Hamburguer', 6) 
-print('{}'.format(list_tupla.index(1)))
+print('\033[1;33m=\033[m' * 50)
+nome = 'PADARIA BARREIRO'
+print(f'{nome: ^50}')
+list_tupla = ('Refrigerante 2l', 10, 'Suco', 5, 'Café com leite', 3, 'Café', 2, 'Pastel', 3, 'Coxinha', 3, 'Enrolado de salsicha', 3, 'Hamburguer', 6) 
+print('\033[1;33m=\033[m' * 50)
+for n in list_tupla:
+    if type(n) is str:
+        print(f'{n:.<42}',end='' )
+    else: 
+        print(f'R${n:.2f}')
+print('\033[1;33m=\033[m' * 50)
